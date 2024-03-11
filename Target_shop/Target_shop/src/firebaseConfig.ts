15cc15ce9a5dc6b664b0a558866a5a34871f3fb9
jeiavonni <jeiavonni765@gmail.com>
2024-03-11 15:33:59 -0500

    Week 9 day 1,2, and 3

---
+++ Target_shop/Target_shop/src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAmQwKmC2tZVE-0j2BgZU-6QnT_eN6upH0",
+    authDomain: "target-shop.firebaseapp.com",
+    projectId: "target-shop",
+    storageBucket: "target-shop.appspot.com",
+    messagingSenderId: "177072076623",
+    appId: "1:177072076623:web:57bea10cb7a3af2b73773a"
+  };
